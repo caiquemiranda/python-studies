42c1fe90d353e388122fc797d77a9bea95c8abbc
caiquemiranda <caiquemirandas@gmail.com>
2022-03-02 22:51:29 -0300

    ex 002

---
+++ exercicios/ex002.py
@@ -0,0 +1,11 @@
+"""
+
+Calculo de aumente de valor de salário, reajuste de 15%
+
+"""
+
+salario = float(input('Qual salário do funcuinário: '))
+novo_salario = salario * 1.15
+
+
+print(f'Após reajuste de 15% o novo salário do funcionario vai ser de R${novo_salario}')
