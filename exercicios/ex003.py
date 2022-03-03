7bed434cb191086c5c945ccd34cedb7c2b62d67c
caiquemiranda <caiquemirandas@gmail.com>
2022-03-02 22:56:07 -0300

    ex003

---
+++ exercicios/ex003.py
@@ -0,0 +1,11 @@
+"""
+
+Conversor de Temperaturas
+
+"""
+
+temperatura_c = float(input('Informe a temperatura em Cº: '))
+
+temperatura_k = temperatura_c + 273.15
+
+print(f'A temperatura{temperatura_c} em Cº corresponde a {temperatura_k} em K')
