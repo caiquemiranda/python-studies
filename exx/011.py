62f5e0004aff7aab4ba732c1386d05ec1fe8beff
caiquemiranda <caiquemirandas@gmail.com>
2022-05-23 16:09:11 -0300

    studies

---
+++ exx/011.py
@@ -0,0 +1,14 @@
+'''
+Leia a idade do usuario e classifique em 
+criança    -> 0-12 anos
+adolecente -> 13-17 anos
+adulto     -> acima de 18 anos
+* se o usuario digitar um numero invalido
+gerar uma msg que a idade é invalida
+
+'''
+
+
+print('=========================')
+idade = int(input('Digite sua idade: '))
+print('=========================')
