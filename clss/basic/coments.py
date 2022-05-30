345b2ea0c9521e41225b972d3a929c950a507d27
caiquemiranda <caiquemirandas@gmail.com>
2022-05-29 21:12:12 -0300

    comments-code

---
+++ clss/basic/coments.py
@@ -0,0 +1,20 @@
+
+
+# Apenas uma linha
+
+
+'''
+
+(aspas simples)
+
+Todo codigo contido dentro das aspas triplas
+
+'''
+
+"""
+
+(aspas duplas)
+
+Todo codigo contido dentro das aspas triplas
+
+"""
