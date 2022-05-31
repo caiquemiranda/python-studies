f2288f7dcb1ff9bba9147548c27bddf5c16ddde0
caiquemiranda <caiquemirandas@gmail.com>
2022-05-31 06:51:44 -0300

    exx

--- exx/008.py
+++ exx/008.py
@@ -1,4 +1,23 @@
 '''
-5. Quais estruturas de laço o Python suporta? Dê um exemplo simples de uso apropriado para cada tipo
+5. Quais estruturas de laço o Python suporta? 
+Dê um exemplo simples de uso apropriado para cada tipo
 de laço.
 '''
+
+# sabe o numero de iterações
+# for (range)
+# tamaho de lista
+lista = [1, 10, 100, 1_000, 10_000]
+for i in range(len(lista)):
+    print(i)
+
+# intervalo 0->5
+for i in range(0, 5):
+    print(i)
+
+# não sabe o numero de iterações
+# while (conditions)
+n=0
+while (n <= 5):
+    print(n)
+    n +=1
--- exx/009.py
+++ exx/009.py
@@ -1,4 +1,5 @@
 '''
+
 6. Quantas iterações os seguintes laços executam?
 a. for i in range(1, 11) . . .
 b. for i in range(10) . . .
@@ -7,4 +8,30 @@ d. for i in range(−10, 11) . . .
 e. for i in range(10, 0) . . .
 f. for i in range(−10, 11, 2) . . .
 g. for i in range(−10, 11, 3) . . .
+
 '''
+
+
+a = 0
+for i in range(1, 11):
+    a +=1
+b = 0
+for i in range(10):
+    b +=1
+c = 0
+for i in range(10, 0, -1):
+    c +=1
+d = 0
+for i in range(-10, 11):
+    d +=1
+e = 0
+for i in range(10, 0):
+    e +=1
+f = 0
+for i in range(-10, 11, 2):
+    f +=1
+j = 0
+for i in range(-10, 11, 3):
+    j +=1
+
+print(a, b, c, d, e, f, j)
