f80ea620aca68dcb7a39255dfaa6bafe16528725
caiquemiranda <caiquemirandas@gmail.com>
2022-06-14 17:01:35 -0300

    python-calc-par

--- exx/005.py
+++ exx/005.py
@@ -1,8 +1,13 @@
 '''
+
 2. Escreva um laço que calcule
 a. A soma de todos os números pares entre 2 e 100 (inclusive).
 b. A soma de todos os quadrados entre 1 e 100 (inclusive).
 c. A soma de todos os números ímpares entre a e b.
 d. A soma de todos os dígitos ímpares de n. (Por exemplo, se n for 32677, a soma deve ser 3 + 7 + 7 =
 17).
+
 '''
+
+for i in range(1, 101):
+    print ('i')
